1ee9f8aec448bf9e9465980a33e1127396eb5dac
Anders Björkland <git@andersbjorkland.online>
2020-12-15 11:05:50 +0100

    Added transformation for hamburger menu.

--- src/Components/HamburgerIcon/HamburgerIcon.styles.js
+++ src/Components/HamburgerIcon/HamburgerIcon.styles.js
@@ -1,23 +1,45 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  --height: 1.6rem;
+  --width: 2rem;
+  
   display: flex;
   flex-direction: column;
   position: relative;
   justify-content: space-between;
-  height: 1.6rem;
+  height: var(--height);
   div {
     border-radius: 0.2rem;
     background-color: var(--blue);
     height: 0.25rem;
-    width: 2rem;
+    width: var(--width);
+    
+    transition: 0.5s;
+    
+    &:nth-child(1) {
+      transform-origin: left;
+    }
+    &:nth-child(2) {
+      transform-origin: center;
+    }
+    &:nth-child(3) {
+      transform-origin: left;
+    }
   }
   &.active {
     div {
       &:nth-child(1) {
-        transform: rotateX(45deg);
+        transform: rotate(45deg);
+        width: ${1.6/Math.sin(45)}rem;
       }
-      background-color: red;
+      &:nth-child(2) {
+        transform: scaleX(0);
+      }
+      &:nth-child(3) {
+        transform: rotate(-45deg);
+        width: ${1.6/Math.sin(45)}rem;
+      }
     }
   }
 `;
